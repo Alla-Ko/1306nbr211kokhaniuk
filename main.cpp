7fe6e501ba8e46c10ee7502d76c5cb12b515826f
Alla-Ko <alla.kokhaniuk@gmail.com>
2023-08-26 09:28:38 +0000

    test

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main() {
     int n=16;
     for (int i = 2; i <= n; i++)
     {
-        cout << "Останні 2 цифри числа 6 в степені " << i << " :" << Charivne_6(i) << endl;
+      cout << "!!!";
+      cout << "Останні 2 цифри числа 6 в степені " << i << " : " << Charivne_6(i) << endl;
     }
     cout << endl;
   
 }
+//fghjkl;
 //дві останні цифри результату піднесення числа 6 до степеня .
 int Charivne_6(int n)
 {
