751f8866b3d4ae6a4fc311d315a3484d583d2281
Alla-Ko <121404468+Alla-Ko@users.noreply.github.com>
2023-08-26 09:17:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@1306nbr211kokhaniuk/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int Charivne_6(int n);
+
+int main() {
+
+    int n=16;
+    for (int i = 2; i <= n; i++)
+    {
+        cout << "Останні 2 цифри числа 6 в степені " << i << " :" << Charivne_6(i) << endl;
+    }
+    cout << endl;
+  
+}
+//дві останні цифри результату піднесення числа 6 до степеня
+int Charivne_6(int n)
+{
+    
+    int res = (long long)pow(6, n) % 100;
+    return res;
+}
